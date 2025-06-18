0db3f3f4571e310609a47d1f0f6e3b7b379f2f14
Anton <kesya@gmail.com>
2025-06-18 23:24:24 +0500

    layout and main started

--- client-FP/src/app/App.jsx
+++ client-FP/src/app/App.jsx
@@ -1,17 +1,16 @@
 import Layout from "./components/layout/Layout.component"
+import Main from "./pages/main/Main.component"
 import "./styles.scss"
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
-    {}
+    { path: "/", element: <Main /> }
 ])
 
 export default function App() {
     return (
         <Layout>
-            {/* <Layout> */}
-                {/* RouterProvider */}
-            {/* </Layout> */}
+            <RouterProvider router={router} />
         </Layout>
     )
 }
---
+++ client-FP/src/app/pages/main/Main.component.jsx
@@ -0,0 +1,24 @@
+import "./main.scss"
+
+export default function Main() {
+    return (
+        <>
+            <section className="main">
+                <div className="container">
+                    <div className="main-title">
+                        Bookshelf <br />
+                        Первый Буккроссинг в Тюмени
+                    </div>
+                    <div className="main-text">
+                        Если вы страстный любитель чтения или коллекционер книг, то вам - к нам!
+                        <br />
+                        Уникальные возможности, которые ни стоят ни копейки:
+                        <br /> - Обмениваться книгами онлайн
+                        <br /> - Собирать раритетную букинистику
+                        <br /> - Читать и выкладывать электронные и бумажные книги
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
